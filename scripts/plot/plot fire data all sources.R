84ae159daf5db0baf8b65a5cc9a2c6479dcab925
Aimee Chhen <105258138+aimeechhen@users.noreply.github.com>
2024-12-09 12:18:55 -0800

    change fire dates plot fire data and moving window results file location

--- scripts/plot/plot fire data all sources.R
+++ scripts/plot/plot fire data all sources.R
@@ -11,7 +11,7 @@ library(lubridate)
 
 
 # load collar data and shapefile
-source('./scripts/collar_data_and_shapefile.r')
+source('./scripts/source/collar_data_and_shapefile.r')
 
 # calculate the center point of the collar data based on all the data points. st_union is used due to multiple points
 centroid <- st_centroid(st_union(collar_data_sf))
@@ -20,75 +20,68 @@ area_of_interest <- st_buffer(centroid, dist = 50000)
 # Create a geometry for the area of interest
 area_of_interest <- st_as_sfc(st_bbox(area_of_interest))
 
-
+ext(collar_data_sf)
 
 ggplot() +
-  geom_point(data = collar_data, aes(x = longitude, y = latitude), shape = 6)
+  geom_point(data = collar_data, aes(x = longitude, y = latitude , color = collar_id))
 
 # load cathedral park shapefile
-source('./scripts/cathedral_park_shapefile.r')
+source('./scripts/source/cathedral_park_shapefile.r')
 
 # # Convert crs from BC Albers to WGS84 Geographic projection (i.e. "latitude/longitude projection")
 cathedral <- st_transform(cathedral, "+proj=longlat +datum=WGS84")
 
 ggplot() +
   geom_sf(data = cathedral, fill = NA, color = "black", size = 1, lwd = 0.5) +
-  geom_sf(data = area_of_interest, fill = NA, color = "purple", size = 1, lwd = 0.5)
+  geom_sf(data = area_of_interest, fill = NA, color = "green", size = 1, lwd = 0.5) +
+  geom_point(data = collar_data, aes(x = longitude, y = latitude, color = collar_id))
 
 
 
 
 #.............................................
-# nasa fire data ----
-source('./scripts/nasa fire prep.r')
-
-# combine all 3 nasa satellite instruments into one df
-nasa_fire <- rbind(modis, viirs_suomi)
-nasa_fire <- rbind(nasa_fire, viirs_noaa)
-
-# plot data
-ggplot() +
-  geom_sf(data = nasa_fire)
-
+# # nasa fire data ----
+# source('./scripts/source/nasa fire prep.r')
+# 
+# # combine all 3 nasa satellite instruments into one df
+# nasa_fire <- rbind(modis, viirs_suomi)
+# nasa_fire <- rbind(nasa_fire, viirs_noaa)
+# 
+# # plot data
 # ggplot() +
-#   geom_sf(data = modis, aes(color = "MODIS")) +
-#   geom_sf(data = viirs_suomi, aes(color = "VIIRS Suomi")) +
-#   geom_sf(data = viirs_noaa, aes(color = "VIIRS NOAA")) +
-#   scale_color_manual(values = c("MODIS" = "red", "VIIRS Suomi" = "purple", "VIIRS NOAA" = "orange"))
-
-ggplot() +
-  geom_sf(data = nasa_fire, aes(color = "NASA")) +
-  scale_color_manual(values = c("NASA" = "red"))
+#   geom_sf(data = nasa_fire)
+# 
+# # ggplot() +
+# #   geom_sf(data = modis, aes(color = "MODIS")) +
+# #   geom_sf(data = viirs_suomi, aes(color = "VIIRS Suomi")) +
+# #   geom_sf(data = viirs_noaa, aes(color = "VIIRS NOAA")) +
+# #   scale_color_manual(values = c("MODIS" = "red", "VIIRS Suomi" = "purple", "VIIRS NOAA" = "orange"))
+# 
+# ggplot() +
+#   geom_sf(data = nasa_fire, aes(color = "NASA")) +
+#   scale_color_manual(values = c("NASA" = "red"))
 
 #.............................................
 # goes fire data ----
-
-# source('./scripts/goes fire prep.r')
+# 
+# source('./scripts/source/goes fire prep.r')
 # goes <- st_transform(goes, "EPSG:3005")
 # 
-# Draw outline i.e outline_unsmoothed
+# # Draw outline i.e outline_unsmoothed
 # goes$geometry <- st_convex_hull(goes$geometry)
-
-
+# 
+# 
 # ggplot() +
 #   geom_sf(data = goes, aes(color = "Goes"), fill = NA) +
 #   scale_color_manual(values = c("Goes" = "darkred"))
-
-
+# 
+# 
 
 
 #.............................................
 # foippa fire data ----
 
-FOIPPA <- st_read('data/fire/bc_gov_FOIPPA/crater_boundaries/23 K52125 Perimeter History Jun17.shp')
-FOIPPA <- st_transform(FOIPPA, crs = 4326)
-
-colnames(FOIPPA)[colnames(FOIPPA) == 'CaptureDat'] <- 'date'
-colnames(FOIPPA)[colnames(FOIPPA) == 'CaptureTim'] <- 'time'
-FOIPPA$date <- as.Date(FOIPPA$date, format = '%Y-%m-%d')
-FOIPPA$time <- format(strptime(FOIPPA$time, format = "%H%M"), format = "%H:%M:%S")
-# combine date and time into timestamp
-FOIPPA$timestamp <- ymd_hms(paste(FOIPPA$date, FOIPPA$time))
+source('scripts/foippa fire prep.r')
 
 # Remove perimeter that has a same date as another
 # FOIPPA <- FOIPPA[!FOIPPA$Source == 9,]
@@ -103,10 +96,18 @@ ggplot() +
 
 # plot each perimeter/dat
 ggplot() +
-  geom_sf(data = FOIPPA, aes(color = date), fill = NA, lwd = 0.5) +
-  scale_color_manual(name = "date", values = rainbow(length(unique(FOIPPA$date)))) +
-  labs(title = "FOIPPA Fire Polygons for Crater Creek",
-       x = "Longitude", y = "Latitude")
+  geom_point(data = collar_data, aes(x = longitude, y = latitude), color = "black") +
+  geom_sf(data = FOIPPA, aes(color = as.factor(date)), fill = NA, lwd = 0.7) +
+  scale_color_manual(name = "Fire boundary", values = rainbow(length(unique(FOIPPA$date)))) +
+  theme_bw() +
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank())
+
+ggsave(last_plot(),  file="figures/goat_foippa.png",
+       width = 6, height = 6.23, units = "in", dpi = 600, bg = "transparent")
+
+
+
 
 ggplot() +
   geom_sf(data = FOIPPA, aes(color = date), fill = NA, lwd = 0.5) +
@@ -120,18 +121,17 @@ ggplot() +
 
 ggplot() +
   geom_sf(data = cathedral, color = "black", fill = NA, size = 1, linetype = 'dashed') +
-  geom_point(data = collar_data, aes(x = longitude, y = latitude, color = "Mountain Goats"), fill = "darkgrey", shape = 25) + 
-  # geom_sf(data = modis, aes(color = "MODIS")) +
-  # geom_sf(data = viirs_suomi, aes(color = "VIIRS Suomi")) +
-  # geom_sf(data = viirs_noaa, aes(color = "VIIRS NOAA")) +
-  geom_sf(data = nasa_fire, aes(color = "NASA")) +
-  geom_sf(data = FOIPPA, aes(color = "FOIPPA"), fill = NA, lwd = 0.5) +
-  # geom_sf(data = goes, aes(color = "Goes"), fill = NA) +
-  scale_color_manual(values = c("Mountain Goats" = "darkgrey",
-                                "NASA" = "red", 
-                                # "VIIRS Suomi" = "purple", "VIIRS NOAA" = "orange",
-                                # "Goes" = "darkred",
-                                "FOIPPA" = "blue")) +
+  geom_point(data = collar_data, aes(x = longitude, y = latitude, color = collar_id)) + 
+  # geom_sf(data = modis, color = "pink")) +
+  # geom_sf(data = viirs_suomi, color = "pink")) +
+  # geom_sf(data = viirs_noaa, color = "pink")) +
+  geom_sf(data = nasa_fire, color = "pink") +
+  geom_sf(data = FOIPPA, color = "blue", lwd = 0.5, fill = NA) +
+  geom_sf(data = goes, color= "red", lwd = 0.5, fill = NA) +
+  # scale_color_manual(values = c("NASA" = "red", 
+  #                               # "VIIRS Suomi" = "purple", "VIIRS NOAA" = "orange",
+  #                               # "Goes" = "darkred",
+  #                               "FOIPPA" = "blue")) +
   theme_bw() +
   theme(panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
@@ -152,8 +152,8 @@ ggsave(last_plot(), filename = "./figures/fire_data_5_goat_nasa_foippa.png",
 #___________________________________________________________________
 # animate
 
-fire_start <- '2023-07-15'
-fire_end <- '2023-09-30'
+fire_start <- '2023-07-22'
+fire_end <- '2023-10-26'
 
 
 # modis <- subset(modis, 
@@ -167,17 +167,19 @@ collar_data <- subset(collar_data,
                       collar_data$date >= fire_start & collar_data$date <= fire_end)
 
 # animate
-p <- ggplot() +
+p <- 
+  ggplot() +
   geom_sf(data = cathedral, color = "black", fill = NA, linewidth = 1) +
-  geom_point(data = collar_data, aes(x = longitude, y = latitude, color = "Mountain Goats"), shape = 25) + 
+  geom_point(data = collar_data, aes(x = longitude, y = latitude, color = collar_id)) + 
   # geom_sf(data = nasa_fire, aes(color = "NASA", fill = Shape_Area)) +
-  geom_sf(data = nasa_fire, aes(color = "NASA")) +
-  # geom_sf(data = goes, color = "GOES", linewidth = 1.5, alpha = 0.3) +
-  geom_sf(data = FOIPPA, aes(color = "FOIPPA"), fill = NA, linewidth = 1.5) +
-  scale_color_manual(values = c("Mountain Goats" = "darkgrey",
-                                "NASA" = "red", 
-                                # "GOES" = "purple",
-                                "FOIPPA" = "blue")) +
+  # geom_sf(data = nasa_fire, aes(color = "NASA")) +
+  geom_sf(data = goes, color = "red", linewidth = 1.5, alpha = 0.3) +
+  # geom_sf(data = FOIPPA, aes(color = "FOIPPA"), fill = NA, linewidth = 1.5) +
+  # scale_color_manual(values = c("Mountain Goats" = "darkgrey",
+  #                               # "NASA" = "red", 
+  #                               # "GOES" = "purple",
+  #                               # "FOIPPA" = "blue"
+  #                               )) +
   labs(title = "Fire data for Crater Creek",
        # fill = "Shape Area",
        subtitle = "Date: {frame_time}",
