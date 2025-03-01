cfa99077e40e243f3d33136b5e559a895caba29d
Aimee Chhen <105258138+aimeechhen@users.noreply.github.com>
2025-02-28 19:35:13 -0800

    clean repo, organize scripts

--- scripts/02_fire_data.R
+++ scripts/02_fire_data.R
@@ -5,8 +5,8 @@
 source('./scripts/source/nasa fire prep.r')
 
 # combine all 3 nasa satellite instruments into one df
-nasa_fire <- rbind(modis, viirs_suomi)
-nasa_fire <- rbind(nasa_fire, viirs_noaa)
+firms <- rbind(modis, viirs_suomi)
+firms <- rbind(firms, viirs_noaa)
 
 # goes fire data ----
 source('./scripts/source/goes fire prep.r')
