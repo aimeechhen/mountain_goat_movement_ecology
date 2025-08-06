23b2ad695463d23b3f0bae12c47f44635f385535
Aimee Chhen <105258138+aimeechhen@users.noreply.github.com>
2025-08-06 15:03:06 -0700

    added rsf yearly mean population level and rsf plots to rsf analysis script

--- scripts/10_rsf_analysis.R
+++ scripts/10_rsf_analysis.R
@@ -1,20 +1,35 @@
 # rsf analysis
 
 
+library(ctmm)
 library(ggplot2)
 library(gridExtra)
+library(dplyr)
+library(ggpubr)
 
 # import data ----
-rsf_results <- read.csv("./data/combined_data_rsf_results_20250302.csv")
-rsf_yearly_mean <- read.csv( "./data/rsf/combined_data_rsf_yearly_mean_results.csv")
+load("./data/rsf/rsf_results_20250505.rda")
 
 rsf_results$collar_id <- as.factor(rsf_results$collar_id)
 rsf_results$goat_name <- as.factor(rsf_results$goat_name) 
 rsf_results$year <- as.factor(rsf_results$year) 
 
-rsf_yearly_mean$year <- as.factor(rsf_yearly_mean$year) 
+
+#...............................................................................
+## yearly rsf values i.e. mean value per year ----
+
+names(rsf_results)
+# set columns for mean calculations
+rsf_columns <- c("rsf_elev_min", "rsf_elev_est", "rsf_elev_max", "rsf_elev_cov","rsf_dist_escape_min", "rsf_dist_escape_est", "rsf_dist_escape_max", "rsf_dist_escape_cov")
+
+# calculate mean values for each year
+rsf_yearly <- aggregate(. ~ year, data = rsf_results[, c("year", rsf_columns)], FUN = mean, na.rm = TRUE)
 
 
+# rsf coefficent values
+# Values near 0 = no preference
+# Values below 0 = selected for, to be closer to escape terrain
+# values above 0 = selected against
 
 
 
@@ -25,96 +40,109 @@ rsf_yearly_mean$year <- as.factor(rsf_yearly_mean$year)
 
 year_palette <- c("#b3b3b3", "#b3b3b3","#b3b3b3", "#b3b3b3", "#ae2012", "#b3b3b3") 
 data_years <- as.factor(c('2019', '2020', '2021', '2022', '2023', '2024'))
-# data_years <- as.factor(seq(from = 2019, to = 2024, by = 1))
+# data_years <- as.factor(seq(from = 2019, to = 2024, by = 1)) # does the same thing as the line above, but line above allows you to see the years and the colour to match it explicitly
+# set the year order based on data_years
+# rsf_results$year <- factor(rsf_results$year, levels = data_years)
+
+#...............................................................................
+# RSF elevation ----
+# Plot elevation across all years, grouped by individual
+
+pd = position_dodge(width = 0.5)
+
+plot_rsf_elevation <-
+ggplot(data = rsf_results) +
+  geom_hline(yintercept = 0, col = "grey70", linetype = "dashed") +
+  geom_pointrange(aes(x = goat_name, y = rsf_elev_est, ymin = rsf_elev_min, ymax = rsf_elev_max,
+                      colour = year),
+                  size = 0.5, position = pd) +
+  ylab("Elevation") +
+  ggtitle("a)") +
+  theme_bw() +
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        plot.title = element_text(hjust = -0.05, size = 10, family = "sans"),
+        axis.title.y = element_text(size=10, family = "sans", face = "bold"),
+        axis.title.x = element_blank(),
+        axis.text.y = element_text(size=8, family = "sans"),
+        axis.text.x  = element_text(size=8, family = "sans"),
+        legend.position = "top", #horizontal, vertical
+        # legend.position.inside = c(0.5, 1.05),
+        legend.justification = "center",
+        legend.direction = "horizontal",
+        legend.title = element_blank(),
+        legend.background = element_blank(),
+        panel.background = element_rect(fill = "transparent"),
+        plot.background = element_rect(fill = "transparent", color = NA)) +
+  scale_colour_manual(values = year_palette,
+                      breaks = data_years) + 
+  # guides(colour = guide_legend(ncol = 1)) # set legend format, if you want it on the side
+guides(colour = guide_legend(nrow = 1)) # set legend format, if you want it top/bot
 
 # 
-# #...............................................................................
-# # RSF elevation ----
-# # Plot elevation across all years
-# 
-# pd = position_dodge(width = 0.5)
-# 
-# # plot_rsf_elevation <-
-# ggplot(data = rsf_results) + 
-#   geom_hline(yintercept = 0, col = "grey70", linetype = "dashed") +
-#   geom_pointrange(aes(x = goat_name, y = rsf_elev_est, ymin = rsf_elev_min, ymax = rsf_elev_max, 
-#                       colour = year_palette), 
-#                   size = 0.5, position = pd) +
-#   ylab("Elevation") +
-#   ggtitle("a)") +
-#   theme_bw() +
-#   theme(panel.grid.major = element_blank(),
-#         panel.grid.minor = element_blank(),
-#         plot.title = element_text(hjust = -0.05, size = 10, family = "sans"),
-#         axis.title.y = element_text(size=10, family = "sans", face = "bold"),
-#         axis.title.x = element_blank(), 
-#         axis.text.y = element_text(size=8, family = "sans"),
-#         axis.text.x  = element_text(size=8, family = "sans"),
-#         legend.position = c(0.5, 1.05), #horizontal, vertical
-#         legend.justification = "center",
-#         legend.direction = "horizontal",
-#         legend.title = element_blank(), 
-#         legend.background = element_blank(),
-#         panel.background = element_rect(fill = "transparent"),
-#         plot.background = element_rect(fill = "transparent", color = NA)) +
-#   scale_colour_manual(values = year_palette, 
-#                       breaks = data_years)
-# 
-# 
 # ggsave(plot_rsf_elevation,
-#        file="figures/individual_plot/fire_goat_rsf_elevation.png",
+#        file="figures/rsf/rsf_elevation_individual.png",
 #        width = 6.86, height = 3, units = "in", dpi = 600, bg = "transparent")
-# 
-# 
-# 
-# #..........................................................
-# # Plot RSF distance to escape ----
-# #..........................................................
-# 
-# 
-# # Plot distance to escape across all years
-# 
-# plot_rsf_dist_esc <-
-#   ggplot(data = results_df) + 
-#   geom_hline(yintercept = 0, col = "grey70", linetype = "dashed") +
-#   geom_pointrange(aes(x = goat_name, y = rsf_dist_escape_est, ymin = rsf_dist_escape_min, ymax = rsf_dist_escape_max, color = year), 
-#                   size = 0.5, position = pd) +
-#   ylab("Distance to Escape Terrain") +
-#   ggtitle("b)") +
-#   theme_bw() +
-#   theme(panel.grid.major = element_blank(),
-#         panel.grid.minor = element_blank(),
-#         plot.title = element_text(hjust = -0.05, size = 10, family = "sans"),
-#         axis.title.y = element_text(size=10, family = "sans", face = "bold"),
-#         axis.title.x = element_blank(), 
-#         axis.text.y = element_text(size=8, family = "sans"),
-#         axis.text.x  = element_text(size=8, family = "sans"),
-#         legend.position="none",
-#         legend.title = element_blank(), 
-#         panel.background = element_rect(fill = "transparent"),
-#         plot.background = element_rect(fill = "transparent", color = NA)) +
-#   scale_colour_manual(values = year_palette, 
-#                       breaks = data_years)
-# 
+
+
+
+#..........................................................
+# Plot RSF distance to escape ----
+#..........................................................
+
+
+# Plot distance to escape across all years, grouped by individual
+
+plot_rsf_dist_esc <-
+  ggplot(data = rsf_results) +
+  geom_hline(yintercept = 0, col = "grey70", linetype = "dashed") +
+  geom_pointrange(aes(x = goat_name, y = rsf_dist_escape_est, ymin = rsf_dist_escape_min, ymax = rsf_dist_escape_max, color = year),
+                  size = 0.5, position = pd) +
+  ylab("Distance to Escape Terrain") +
+  ggtitle("b)") +
+  theme_bw() +
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        plot.title = element_text(hjust = -0.05, size = 10, family = "sans"),
+        axis.title.y = element_text(size=10, family = "sans", face = "bold"),
+        axis.title.x = element_blank(),
+        axis.text.y = element_text(size=8, family = "sans"),
+        axis.text.x  = element_text(size=8, family = "sans"),
+        legend.position="none",
+        legend.title = element_blank(),
+        panel.background = element_rect(fill = "transparent"),
+        plot.background = element_rect(fill = "transparent", color = NA)) +
+  scale_colour_manual(values = year_palette,
+                      breaks = data_years)
+
 # 
 # ggsave(plot_rsf_dist_esc,
-#        file="figures/individual_plot/fire_goat_rsf_dist_esc.png",
+#        file="figures/rsf/rsf_dist_esc.png",
 #        width = 6.86, height = 3, units = "in", dpi = 600, bg = "transparent")
-# 
-# 
-# 
-# #............................................
-# # Multi-panel RSF ----
-# #............................................
-# 
-# plot_rsf <- grid.arrange(plot_rsf_elevation, plot_rsf_dist_esc,
+
+
+
+#............................................
+# Multi-panel RSF ----
+#............................................
+
+# plot_rsf_individual <- grid.arrange(plot_rsf_elevation, plot_rsf_dist_esc,
 #                          ncol = 1)
-# 
-# ggsave(plot_rsf,
-#        file="figures/rsf/fire_goat_rsf.png",
-#        width = 6.86, height = 6, units = "in", dpi = 600, bg = "transparent")
-# 
-# 
+
+plot_rsf_individual <- ggarrange(plot_rsf_elevation, plot_rsf_dist_esc,
+          ncol = 1,
+          common.legend = TRUE,
+          legend = "top")
+plot_rsf_individual
+
+ggsave(plot_rsf_individual, 
+       file="figures/rsf/plot_rsf_individual.png",
+       width = 6.86, height = 6, units = "in", dpi = 600, bg = "transparent")
+
+
+
+
+
 
 
 
@@ -123,13 +151,14 @@ data_years <- as.factor(c('2019', '2020', '2021', '2022', '2023', '2024'))
 # Plot annual RSF elev mean ----
 #..........................................................
 
+# population level
 
-plot_rsf_annual_elev <- 
-ggplot(data = rsf_yearly_mean) +
-       geom_hline(yintercept = 0, col = "black", linetype = "dashed") +
-         geom_pointrange(aes(x = year, y = rsf_elev_est, ymin = rsf_elev_min, ymax = rsf_elev_max, 
-                             color = year), 
-                         size = 0.5, position = pd) +
+plot_rsf_annual_elev <-
+  ggplot(data = rsf_yearly) +
+  geom_hline(yintercept = 0, col = "black", linetype = "dashed") +
+  geom_pointrange(aes(x = year, y = rsf_elev_est, ymin = rsf_elev_min, ymax = rsf_elev_max, 
+                      color = year), 
+                  size = 0.5, position = pd) +
   ylab("Elevation") +
   # ggtitle("a)") +
   theme_bw() +
@@ -141,10 +170,13 @@ ggplot(data = rsf_yearly_mean) +
         axis.text.y = element_text(size=8, family = "sans"),
         axis.text.x  = element_text(size=8, family = "sans"),
         legend.position = "none",
+        legend.title = element_blank(),
         panel.background = element_rect(fill = "transparent"),
         plot.background = element_rect(fill = "transparent", color = NA)) +
   scale_colour_manual(values = year_palette, 
                       breaks = data_years)
+
+
 
 
 
@@ -154,7 +186,7 @@ ggplot(data = rsf_yearly_mean) +
 
 
 plot_rsf_annual_dist_esc <- 
-ggplot(data = rsf_yearly_mean) +
+  ggplot(data = rsf_yearly) +
   geom_hline(yintercept = 0, col = "black", linetype = "dashed") +
   geom_pointrange(aes(x = year, y = rsf_dist_escape_est, ymin = rsf_dist_escape_min, ymax = rsf_dist_escape_max, 
                       color = year), 
@@ -166,10 +198,11 @@ ggplot(data = rsf_yearly_mean) +
         panel.grid.minor = element_blank(),
         plot.title = element_text(hjust = -0.05, size = 10, family = "sans"),
         axis.title.y = element_text(size=10, family = "sans", face = "bold"),
-        axis.title.x = element_blank(), 
+        axis.title.x = element_blank(),
         axis.text.y = element_text(size=8, family = "sans"),
         axis.text.x  = element_text(size=8, family = "sans"),
-        legend.position = "none",
+        legend.position="none",
+        legend.title = element_blank(),
         panel.background = element_rect(fill = "transparent"),
         plot.background = element_rect(fill = "transparent", color = NA)) +
   scale_colour_manual(values = year_palette, 
@@ -183,8 +216,11 @@ ggplot(data = rsf_yearly_mean) +
 #............................................
 
 plot_rsf_annual <- grid.arrange(plot_rsf_annual_elev, plot_rsf_annual_dist_esc,
-                         nrow = 1)
+                                nrow = 1)
+
+plot_rsf_annual
+
 
 ggsave(plot_rsf_annual,
        file="figures/rsf/rsf_annual_mean.png",
-       width = 9.86, height = 3.23, units = "in", dpi = 600)
+       width = 9.86, height = 3.23, units = "in", dpi = 600, bg = "transparent")
