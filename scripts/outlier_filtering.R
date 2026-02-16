743d13ead4dcdc09df1cf63b879d5dce654e4ec8
Chhen <aimee.chhen@ubc.ca>
2026-02-15 21:23:48 -0800

    outlier cleaning (incomplete) before comparing to other goats positions, rechecking version to be committed after

--- scripts/outlier_filtering.R
+++ scripts/outlier_filtering.R
@@ -59,7 +59,6 @@ for (i in 1:length(tel_data)) {
   rownames(tel_data[[i]] ) <- tel_data[[i]]$fix_id
 }
 
-
 # clean up environment
 rm(raw_data, i)
 
@@ -70,25 +69,27 @@ in_progress_folder <- "./data/goat/outlie_filtering/in_progress/20260213/"
 cleaned_folder <- "./data/goat/outlie_filtering/cleaned/20260213/"
 
 # create folder if it doesnt exist
-dir.create(outlie_folder, recursive = TRUE, showWarnings = TRUE)
-dir.create(in_progress_folder, recursive = TRUE, showWarnings = TRUE)
-dir.create(cleaned_folder, recursive = TRUE, showWarnings = TRUE)
-
-# # create folder to store in progress cleaning data
+# dir.create(outlie_folder, recursive = TRUE, showWarnings = TRUE)
+# dir.create(in_progress_folder, recursive = TRUE, showWarnings = TRUE)
+# dir.create(cleaned_folder, recursive = TRUE, showWarnings = TRUE)
 # dir.create("./data/goat/outlie_filtering/in_progress/", recursive = TRUE, showWarnings = TRUE)
 # dir.create("./data/goat/outlie_filtering/cleaned/", recursive = TRUE, showWarnings = TRUE)
 
-
+# # create a list to store all of the potential outliers (only once)
+# identified_list <- vector("list", 10)
+# # assign names to each item
+# names(identified_list) <- names(tel_data)
+# save(identified_list, file = paste0(in_progress_folder, "identified_list.rda"))
 
 
 #...................................................................................
 # outlier filtering
-#.....................................................................................
+#...................................................................................
 
 # run through the whole workflow then start at the top again with a new individual, repeat process
 
 # set individual
-goat_id <- "CA02"
+goat_id <- "CA01"
 # extract data for individual
 goat <- tel_data[[goat_id]]
 # add a column called individual.local.identifier (required for custom functions to work)
@@ -96,8 +97,8 @@ goat$individual.local.identifier <- goat_id
 
 
 #..........................................................
-
 # PART 1: outlie() checks ----
+#..........................................................
 
 cat(green("1. look for outliers, plot & get outlie data \n"))
 # 1. look for outliers, plot & get outlie data
@@ -105,7 +106,7 @@ png(file = paste0(outlie_folder, goat$individual.local.identifier[1], "_1a_raw_c
 out <- plot_outlie(goat) #plot_outlie(x), x is a single individual telemetry ctmm data
 dev.off()
 # #overlay tracking data on a map for better representation to investigate fixes that may be outliers 
-((raw_tracks <- movement_map(goat)))
+# ((raw_tracks <- movement_map(goat)))
 # # save an image of the map
 # mapview::mapshot2(raw_tracks, vwidth = 1600, vheight = 900,
 #                   file = paste0(outlie_folder, goat$individual.local.identifier[1], "_1b_map_raw_tracks.png"))
@@ -159,137 +160,121 @@ saveRDS(out_data, file = paste0(in_progress_folder, goat_id, "_out_data.rds"))
 # PART 2: Manual checks ----
 #/////////////////////////////////////////////////////////////
 
-# # set individual
-# goat_id <- "CA11"
-# goat <- tel_data[[goat_id]]
-# goat$individual.local.identifier <- goat_id
-# 
-# # # import data if not doing all at once
-# good_locations <- readRDS(file = paste0(in_progress_folder, goat_id, "_good_locations.rds"))
-# out_data <- readRDS(file = paste0(in_progress_folder, goat_id, "_out_data.rds"))
-# # extract fix_id of bad location estimates
-# bad_locations <- out_data[out_data$flag_outlier == 1, "fix_id"]
+# set individual
+goat_id <- "CA01"
+goat <- tel_data[[goat_id]]
+goat$individual.local.identifier <- goat_id
+
+# # import data if not doing all at once
+good_locations <- readRDS(file = paste0(in_progress_folder, goat_id, "_good_locations.rds"))
+out_data <- readRDS(file = paste0(in_progress_folder, goat_id, "_out_data.rds"))
+# extract fix_id of bad location estimates
+bad_locations <- out_data[out_data$flag_outlier == 1, "fix_id"]
 movement_map(good_locations, map_type = "Esri.WorldImagery")
 
 cat(yellow(bold("PART 2: Manual checks \n")))
 # create an empty vector to record potential outliers
 potential_outliers <- vector()
 
-#.....................................................................................
+#........................................
 ## CA01 selena_goatmez ----
 
-#point 1: 631 -> bad, 2.25h 2.5km
-#point 2: 1282 -> bad
-#point 3: 1278 -> bad
-#point 4: 2847 -> bad
-#point 5: 346 -> bad
-#point 6: 3207 -> bad
-#point 7: 2483 -> leaving it, 2.5km in 6+ hrs even though this is generally how much they move in an entire day
-# 611 -> leaving it
-# 2978 -> ok, keep, high turn angle but low speed/dist
+# obvious ones
+# 631 -> bad, 2.25h 2.5km
+# 1282 -> bad
+# 2847 -> bad
+# 1278 -> bad
+# 2483 -> okay
+# 346 -> bad
+#less obvious
+# 3207 -> bad
 # 3865 -> leaving it
-# potential_outliers <- c(631, 1282, 1278, 2847, 346, 3207)
+potential_outliers <- c(631, 1282, 2847, 1278, 346, 3207)
 
-#............................
+
+#........................................
 ## CA04 toats_mcgoats ----
 
-#point 1: 17645 -> bad
-#point 2: 15595 -> bad
-#point 3: 17621 -> bad
-#point 4: 14931 -> bad
-#point 5: 18862 -> bad
-#point 6: 21381 -> keep, high turn angle/dist but low speed, points not really clustered
-#point 7: 17659 -> keep, high turn angle, dist is ~1100m but trailing distance among points and lowish speed
-#point 8: 17562 -> bad
-#point 9: 18920 -> bad
-#point 10: 16280 -> okay? at lot of night time moving, keeping
-#point 11: 15170 -> keeping, a lot of early movement
-# 15628 -> bad
-# 15169 -> keep, next point looks questionable as well but dont want to overclean
-# potential_outliers <- c(17645, 15595, 17621, 14931, 18862, 17562, 18920, 15628)
+# starburst
+# 17516 -> 2021-02-09
+# 15466 -> 2020-02-16
+# 17492 -> 2021-02-06
+# 17530 -> 2021-02-10
+# 17433 -> 2021-01-31
+# 18791 -> 2021-06-11
+# 18733 -> 2021-06-06
 
-#............................
+
+#........................................
 ## CA08 goatzilla ----
 
-#point 1: 41176 -> bad, ~180 turn, high dist but low speed 0.13
-#point 2: 34480 -> bad, spike off cluster, ~180
-#point 3: 33878 -> bad, high speed/distance, spike off cluster
-# 34286 -> keep
-# 34253 -> keep
-# potential_outliers <- c(41176, 34480, 33878)
+# 40796
+# 34232
+# 33630 -> leaving it
+# potential_outliers <- c(40796, 34232)
 
-#............................
+#........................................
 ## CA09 the_goatmother ----
 
-#point 1: 46066 -> bad
-#point 2: 43218 -> bad
-#point 3: 48828 -> bad, spike off cluster, far dist
-#point 4: 46278 -> bad
-#point 5: 44100 -> bad 
-#point 6: 41785 -> bad
-#point 7: 43185 -> bad
-#point 8: 45783 -> bad
-#point 9: 41795 -> bad
-#point 10: 43831 -> bad
-#point 11: 45768 -> leaving it, on the edge of flagging
-#point 12: 43927 -> bad
-#point 13: 45390 -> leaving it, on the edge of flagging
-#point 14: 49466 -> leaving it
-#point 15: 43563 -> leaving it
-#point 16: 44594 -> leaving it
-#point 17: 44605 -> okay
-# 41530 -> okay
-# 44007 -> keep
-# potential_outliers <- c(46066, 43218, 48828, 46278, 44100, 41785, 43185, 45783, 41795, 43831, 43927)
+# 42838 -> bad
+# 48448 -> bad
+# 45898 -> bad
+# 45686 -> bad, really far
+# starburst cluster spikes
+# 45388 -> 2021-06-07, leaving it
+# 43720 -> 2020-12-01, bad
+# 41405 -> 2019-10-29, bad, 1.5h for ~2km to get back to the cluster, near 180 turn
+# 42805 -> 2020-06-04, bad
+# 45403 -> 2021-06-09, bad, 9 hours to point, might be missing a fix but not going to assume, removing
+# 41415 -> 2019-10-30, bad, even though its 1 of the spike off a cluster (41405 day before), dist/speed really high
+# lesser obvious
+# 43451 -> 2020-10-26 leaving it, ca11 has similar fix, could be both bad or actually went
+# potential_outliers <- c(42838, 48448, 45898, 45686, 43720, 41405, 42805, 45403, 41415)
 
-#............................
-# CA11 vincent_van_goat ----
+#........................................
+## CA11 vincent_van_goat ----
 
-#point 1: 58955 -> bad
-#point 2: 61848 -> bad
-#point 3: 64178 -> bad
-#point 4: 58528 -> bad
-#point 5: 58097 -> bad
-#point 6: 58124 -> bad
-#point 7: 60973 -> bad
-#point 8: 60778 -> bad
-#point 9: 60772 -> bad
-#point 10: 66041 -> bad
-#point 11: 60946 -> bad
-#point 12: 58104 -> bad
-#point 13: 60695 -> bad
-#point 14: 65323 -> bad
-#point 15: 59818 -> bad
-#point 16: 63581 -> bad
-#point 17: 64820 -> bad
-#point 18: 64819 -> bad
-#point 19: 60742 -> bad
-# potential_outliers <- c(58955, 61848, 64178, 58528, 58097, 58124, 60973, 60778, 60772, 66041, 60946, 58104, 60695, 65323, 59818, 63581, 64820, 64819, 60742)
+# obvious outliers
+# 63676 -> bad
+# 58026 -> bad
+# 57595 -> bad
+# 57622 -> bad
+# 60471 -> bad
+# 65413 -> bad
+# 60444 -> bad
+# 64695 -> leaving it, 12h interval, high dist but low speed, 179 turn
+# 60193 -> bad
+# 58453 -> bad
+# lesser obvious
+# 60276 -> leaving it
+# 57602 -> leaving it
+# 63079 -> leaving it, 6 hr and 12.5 hr, then returns to cluster, low speed
+# 59316 -> leaving it, ca09 had similar fix location, could be both bad or actual fix, avoiding overcleaning
+# 64255 -> leaving it, 12.5 and 6.25h might be missing a fix inbetween, low speed
+# 61346 -> okay
+# potential_outliers <- c(63676, 58026, 57595, 57622, 60471, 65413, 60444, 60193, 58453)
 
 #............................
 # CA12 goatileo  ----
 
-#point 1: 72042 -> bad
-#point 2: 68537 -> bad
-#point 3: 69108 -> bad
-#point 4: 67847 -> bad
-#point 5: 72788 -> bad
-#point 6: 68346 -> bad
-#point 7: 67166 -> leaving it, speed/distance not TOO high but still, 0.1 & 800m, dont want to over clean
-#point 8: 69127 -> bad
-#point 9: 69092 -> leaving it
-#point 10: 71413 -> okay
-#point 11: 67169 -> okay
-#point 12: 67838 -> okay
-# potential_outliers <- c(72042, 68537, 69108, 67847, 72788, 68346, 69127)
-
-
+# obvious ones
+# 71414 -> leaving it, high dist, low speed 0.17m/s, 6.25 hr
+# 67909 -> bad
+# 68480 -> bad
+# 68499 -> leaving it
+# 67219 -> bad
+# 67718 -> bad
+# 72039 -> leaving it, high dist, but low speed with 18.75 interval
+# lesser obvious
+# 67040 -> leaving it
+# potential_outliers <- c(67909, 68480, 67219, 67718)
 
 #.....................................................................................
-# 1b. look for outliers, plot & get outlie data
 # Examine potential outlier ----
-poi_id <- 10599
-point_check <- plot_range(good_locations, fix_id = poi_id, limit = 10, palette = colour_pal) 
+
+# 1b. look for outliers, plot & get outlie data
+poi_id <- 346
+point_check <- plot_range(good_locations, fix_id = poi_id, limit = 10, palette = colour_pal)
 # point_check <- plot_range(updated_locations, fix_id = poi_id, limit = 10, palette = colour_pal)
 # plot section
 movement_map(point_check$data_section, map_type = "Esri.WorldImagery")
@@ -349,61 +334,74 @@ saveRDS(updated_locations, file = paste0(cleaned_folder, goat_id, "_updated_loca
 saveRDS(out_data, file = paste0(cleaned_folder, goat_id, "_out_data_manual_checks.rds"))
 beep(4)
 
+# update to the running list of potential_outliers
+load(identified_list, file = paste0(in_progress_folder, "identified_list.rda"))
+identified_list[[goat_id]] <- c(identified_list, bad_locations, potential_outliers)
+# save updated list for next goat
+save(identified_list, file = paste0(in_progress_folder, "identified_list.rda"))
+
 # move onto next goat, restart the process
-rm(cleaned_tracks, data_section, final_check, goat, good_locations, good_tracks, out, out_data, point_check, raw_tracks, recheck, updated_locations, bad_locations, poi_id, potential_outliers, test, test2)
+rm(cleaned_tracks, data_section, final_check, goat, good_locations, good_tracks, out, out_data, point_check, raw_tracks, recheck, updated_locations, bad_locations, poi_id, potential_outliers, test, test2, identified_list)
 gc()
 dev.off(dev.list()["RStudioGD"])
 
 
 
+  
+  
+  
+  
+  #............................
+  # CA
+  
+  #point 1:  -> 
+  #point 2:  -> 
+  #point 3:  -> 
+  #point 4:  -> 
+  #point 5:  -> 
+  #point 6:  -> 
+  #point 7:  -> 
+  #point 8:  -> 
+  #point 9:  -> 
+  #point 10:  -> 
+  #point 11:  -> 
+  #point 12:  -> 
+  #point 13:  -> 
+  #point 14:  -> 
+  #point 15:  -> 
+  #point 16:  -> 
+  #point 17:  -> 
+  
+  
+  
+  
+  #~~~~~~~~~~~~~~~~~~~~~~~ ----
+# other goats ----
 
-
-
-
-#............................
-# CA
-
-#point 1:  -> 
-#point 2:  -> 
-#point 3:  -> 
-#point 4:  -> 
-#point 5:  -> 
-#point 6:  -> 
-#point 7:  -> 
-#point 8:  -> 
-#point 9:  -> 
-#point 10:  -> 
-#point 11:  -> 
-#point 12:  -> 
-#point 13:  -> 
-#point 14:  -> 
-#point 15:  -> 
-#point 16:  -> 
-#point 17:  -> 
-
-
-
-
-#~~~~~~~~~~~~~~~~~~~~~~~ ----
-# not core 6 goats ----
+#........................................
 # CA02 great goatsby ----
-#point 1: 10837 -> bad
-#point 2: 10208 -> bad
-#point 3: 10373 -> bad
-#point 4: 12505 -> bad
-#point 5: 11078 -> bad
-#point 6: 11074 -> bad
-#point 7: 12149 -> bad
-#point 8: 13251 -> bad
-#point 9: 13813 -> leaving it
-# 10731 -> bad
-# 10728 -> bad
-# 10445 -> leaving it
-# 12083 -> leaving it
-# 12109 -> leaving it
-# 14036 -> bad 
-# potential_outliers <- c(10837, 10208, 10373, 12505, 11078, 11074, 12149, 13251, 10731, 10728)
 
+# obvious ones
+# 10708 -> bad
+# 10949 -> bad
+# 10079 -> bad
+# 10244 -> bad
+# 12376 -> bad
+# 13122 -> bad
+# 13684 -> leaving it
+# lesser obvious
+# 13907 -> bad
+# 10602 -> leaving it, 18h to next fix, these 2 may be behavioural
+# 10599 -> leaving it, these 2 may be behavioural
+# 10224 -> okay
+# 11809 -> ok
+# 12020 -> leaving it, these 4 may be behavioural, over a short period, not random dates
+# 11980 -> leaving it, these 4 may be behavioural, over a short period, not random dates
+# 11954 -> leaving it, these 4 may be behavioural, over a short period, not random dates
+# 10316 -> leaving it, these 4 may be behavioural, over a short period, not random dates (except this one, occurred way earlier
+# potential_outliers <- c(10708, 10949, 10079, 10244, 12376, 13122, 13907)
+
+#........................................
 # CA07 alpine_pacino ----
 # clean, nothing additional
 
