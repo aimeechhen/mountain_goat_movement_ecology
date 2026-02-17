977ce6a2782db3acec7f8c597668770b1f2aa1d3
Chhen <aimee.chhen@ubc.ca>
2026-02-17 02:22:40 -0800

    updated the 1.8 data processing script, check the outlie filtering results, data carpentry of cleaned data

--- scripts/1.8_data_processing.R
+++ scripts/1.8_data_processing.R
@@ -1,141 +1,103 @@
 
-
-
-# import outlie data
-# load("./data/goat/cleaning/in_progress/out_data_good_loc_list_original_final.rda")
-load("./data/goat/cleaning/out_data_good_loc_list.rda")
-
-# convert into a df
-outlie_data <- do.call(rbind, out_data_good_loc_list)
-# reorder based on fix_id
-outlie_data <- outlie_data[order(outlie_data$fix_id),]
-
-
-# load("./data/goat/cleaning/in_progress/out_data_good_loc_list_new_final.rda")
-# outlie_new <- do.call(rbind, out_data_good_loc_list)
-# outlie_new <- outlie_new[order(outlie_new$fix_id),]
-# 
-# # check for same column names to avoid duplication,
-# intersect(colnames(outlie_original), colnames(outlie_new))
-# 
-# # merge the two df
-# outlie_data <- merge(outlie_original, outlie_new, 
-#                      by = c("timestamp", "longitude", "latitude", "class", "HDOP", 
-#                             "fix_id", "goat_id", "goat_name", "collar_id", "data_source", 
-#                             "x", "y","VAR.xy", "individual.local.identifier", "t", 
-#                             "distance", "VAR.distance", "speed", "VAR.speed", "row_n","flag_outlier"), 
-#                      all = TRUE)
-
-# outlie_data <- outlie_data[order(outlie_data$fix_id),]
-# reset rownames based on fix_id
-rownames(outlie_data) <- outlie_data$fix_id
-
-# how many were detected as bad locations?
-sum(outlie_data$flag_outlier) # 1178, 0.5 = 13
-# outlie_data[outlie_data$flag_outlier == 1,]
-save(outlie_data, file = "./data/goat/prep/outlie_data.rda")
-
-load("./data/goat/prep/outlie_data.rda")
+library(ctmm)
+options(scipen = 999)
 
 # import data
 load("./data/goat/prep/combined_data.rda")
 
-# check for same column names to avoid duplication,
-intersect(colnames(combined_data), colnames(outlie_data))
+# cannot calibrate the fixes due to calibration data is only of 1 fix type, cannot apply error models without a lot of additional work
+# therefore, not including location error (i.e., fix type, dop and UERE)
+# need to drop certain columns because not including measurement error, if they are included you will run into issues when converting into ctmm object
+raw_data <- subset(combined_data, select = -c(fix_type, hdop, vdop, pdop, dop, altitude_m))
 
-# combine dfs by fix_id without duplicating the columns when using all = TRUE, cannot list all the same columns in the by= argument, it comes with 154k+ obs.
-collar_data <- merge(combined_data, outlie_data[, !names(outlie_data) %in% names(combined_data)[names(combined_data) != "fix_id"]],
-  by = "fix_id", all = TRUE)
+# convert to ctmm object
+tel_data <- as.telemetry(raw_data, 
+                         mark.rm = TRUE,
+                         keep = c("fix_id", "goat_id", "goat_name", "collar_id", "data_source"))
 
 
-collar_data <- collar_data[order(collar_data$fix_id),]
-rownames(collar_data) <- collar_data$fix_id
+# ensure each df in the list is in the correct order with matching row name
+for (i in 1:length(tel_data)) {
+  # order the df based on fix_id
+  tel_data[[i]] <- tel_data[[i]][order(tel_data[[i]][["fix_id"]]),]
+  # set the rowname to match the fix_id
+  rownames(tel_data[[i]] ) <- tel_data[[i]]$fix_id
+}
 
-str(collar_data)
-# check to see if the flag_outlier transferred over
-# how many were detected as bad locations?
-sum(collar_data$flag_outlier, na.rm = TRUE) #13
-table(collar_data$flag_outlier) # 1178
-range(collar_data$flag_outlier, na.rm = TRUE)
+# clean up environment
+rm(raw_data, i)
+
+
+in_progress_folder <- "./data/goat/outlie_filtering/in_progress/20260213/"
+cleaned_folder <- "./data/goat/outlie_filtering/cleaned/20260213/"
+
+
+#...................................................
+# Check clean data ----
+
+# Load all outlie data
+rds_files <- list.files(cleaned_folder, pattern = ".*out_data.*\\.rds$", full.names = TRUE)
+# Import/read all the files into a list
+rds_list <- lapply(rds_files, readRDS)
+# combine into df 
+outlie_df <- do.call(rbind, rds_list)
+
+# dont need this actually, updated the flag outlier column during the filtering process
+# load(file = paste0(in_progress_folder, "identified_list.rda"))
+# flag_fix_id <- do.call(c, identified_list) #29
+
+# go to visualize_data.R script to plot to check if anything was missed
+
+# save outlie data
+save(outlie_df, file = paste0(cleaned_folder, "outlie_df.rda"))
+
+# clean up environment
+rm(rds_files, rds_list)
+
+#............................................................
+# update tracking data ----
+#............................................................
+
+load(file = paste0(cleaned_folder, "outlie_df.rda"))
+
+# check for same column names to avoid duplication
+intersect(colnames(combined_data), colnames(outlie_df))
+
+# combine df
+clean_data <- merge(combined_data, outlie_df, 
+                    by = c("fix_id", "goat_id", "goat_name", "collar_id", "longitude", "latitude", 
+                           "data_source","individual.local.identifier", "timestamp"), 
+                    all.x = TRUE)
+
+# how many currently outlier from screening process
+sum(clean_data$outlier) #742
+# how many were detected as bad locations and manual checks
+sum(clean_data$flag_outlier, na.rm = TRUE) #29
 # update the column, if it contains NA then give it a 0
-collar_data$flag_outlier <- ifelse(is.na(collar_data$flag_outlier), 0, collar_data$flag_outlier)
+clean_data$flag_outlier <- ifelse(is.na(clean_data$flag_outlier), 0, clean_data$flag_outlier)
+# update the outlier column with bad locations and manual checked outliers
+clean_data$outlier <- ifelse(clean_data$flag_outlier == 1, 1, clean_data$outlier)
+sum(clean_data$outlier) #771
+# 742+29
 
-# how many currently 
-sum(collar_data$outlier) # 742
+# reorganize the df based on combined_data structure
+# check what columns match from the main df and order it in that way and what columns dont match are the leftovers and placed at the back/end of the df
+clean_data <- clean_data[, c(intersect(names(combined_data), names(clean_data)), 
+                             setdiff(names(clean_data), names(combined_data)))]
 
-# update outlier column
-collar_data$outlier <- ifelse(collar_data$flag_outlier == 1, 1, collar_data$outlier)
-sum(collar_data$outlier) #1920, 755
-# 1178+742 #matches
-742+13
-
-
-
-
-save(collar_data, file = "./data/goat/collar_data.rda")
-# go to stage 3. revisualize the good locations to compare the difference and as a reference to see if there are any potential outliers or are those points where others have gone as well across all data
-
-# clean environment
-rm(list = ls())
-gc()
-
-
-
-
-
-
-
-#////////////////////////////////////////////////////////////////////////////////////////////
-# data prep for analysis
-
-library(lubridate)
-
-load("./data/goat/collar_data.rda")
-
-# add temporal attributes
-collar_data$year_local <- year(collar_data$timestamp_local)
-collar_data$month_local <- month(collar_data$timestamp_local, label = FALSE) #label = false for numerical month
-collar_data$day_local <- day(collar_data$timestamp_local)
-collar_data$month_day_local <- format(collar_data$timestamp_local, "%m-%d")
-collar_data$doy_local <- yday(collar_data$timestamp_local) #day of the year
-
-# create a column to be used for period specific analyses
-collar_data$id_year <-  paste(collar_data$goat_id, collar_data$year_local, sep = "_")
+# ensure the df ordered properly
+clean_data <- clean_data[order(clean_data$fix_id),]
+# reset the row number to proper default
+rownames(clean_data) <- NULL
 
 # make a list of goats that were alive during the fire
 goats <- c("CA01", "CA04", "CA08", "CA09", "CA11", "CA12")
 
 # indicate that they are part of the fire study
-collar_data$fire_study <- ifelse(collar_data$goat_id %in% goats, 1, 0)
-
-# Define the dates for wildfire period as per bc wildfire dates
-# July 22 to October 26, 2023
-fire_start <- "2023-07-22"
-fire_end <- "2023-10-26"
-
-# Define the the study period based on the fire period across all years (same days across all years)
-# July 22 to October 26
-period_start <- "07-22"
-period_end <- "10-26"
-
-# create a column to indicate if the fix fall within the fire period, 1 = yes, 0 = no
-collar_data$fire_period <- ifelse(collar_data$date_local >= fire_start & collar_data$date_local <= fire_end, 1, 0)
-
-# create a column to indicate if the fix fall within the study period, 1 = yes, 0 = no
-# study period = the fire period across all years
-collar_data$study_period <- ifelse(collar_data$month_day_local >= period_start & collar_data$month_day_local <= period_end, 1, 0)
-
-#subset to the 6 fire goats
-goat_data <- collar_data[collar_data$goat_id %in% goats,]
-
-# update goat id, name and collar id, individual.local.identifier to only contain 6 levels
-goat_data$goat_id <- droplevels(goat_data$goat_id)
-goat_data$goat_name <- droplevels(goat_data$goat_name)
-goat_data$collar_id <- droplevels(goat_data$collar_id)
-goat_data$individual.local.identifier <- droplevels(goat_data$individual.local.identifier)
-
-# make sure its in the right order
-goat_data <- goat_data[order(goat_data$fix_id), ]
+clean_data$fire_study <- ifelse(clean_data$goat_id %in% goats, 1, 0)
 
 
-save(goat_data, file = "./data/goat/goat_data.rda")
+
+save(clean_data, file = "./data/goat/clean_data.rda")
+
+rm(outlie_df)
