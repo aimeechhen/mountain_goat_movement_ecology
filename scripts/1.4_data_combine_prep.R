30bdaa81976100794819fd752a8611535823cf0e
Chhen <aimee.chhen@ubc.ca>
2026-02-17 01:48:51 -0800

    added last date of original dataset code, drop duplicate fixes, added dop section to df, deleted merging tel_data together code

--- scripts/1.4_data_combine_prep.R
+++ scripts/1.4_data_combine_prep.R
@@ -1,122 +1,115 @@
 # prep data for outlier filtering
-library(ctmm)
 library(dplyr)
 
 
 #import data
 load("./data/goat/prep/raw_original_screened_prepped.rda") #66099
 load("./data/goat/prep/raw_new_prepped.rda")
-load("./data/goat/prep/uere.rda")
 
-# check for same column names to avoid duplication, add goat info to the raw data
+# check end of original data
+aggregate(timestamp_utc ~ goat_id, data = raw_original, max)
+
+# goat_id       timestamp_utc
+# 1     CA01 2023-10-05 06:30:38 *
+# 2     CA02 2021-09-07 11:00:38
+# 3     CA04 2023-10-03 04:30:38 *
+# 4     CA07 2023-04-19 08:57:57
+# 5     CA08 2023-10-05 06:30:38 *
+# 6     CA09 2023-10-04 05:30:38 *
+# 7     CA10 2022-07-07 15:05:20
+# 8     CA11 2023-10-05 06:30:38 *
+# 9     CA12 2023-10-03 10:45:37 *
+# 10    CA18 2021-03-16 23:45:44
+
+# check start of new data
+aggregate(timestamp_utc ~ goat_id, data = raw_new, min)
+# goat_id       timestamp_utc
+# 1    CA01 2023-09-01 03:45:00
+# 2    CA04 2023-09-01 16:16:00
+# 3    CA08 2023-09-01 03:45:00
+# 4    CA09 2023-09-01 03:47:00
+# 5    CA11 2023-09-01 03:45:00
+# 6    CA12 2023-09-01 03:45:00
+# ~ 1 month of overlap
+
+# do not include any fixes that overlap with the original data (i.e., chop off some of the start part of the new data) to avoid duplicates
+# check the data before chopping, these will be duplicated fixes
+dupe_CA01 <- raw_new[raw_new$goat_id == "CA01" & raw_new$timestamp < "2023-10-05 06:30:38", ] #129
+dupe_CA04 <- raw_new[raw_new$goat_id == "CA04" & raw_new$timestamp < "2023-10-03 04:30:38", ] #119
+dupe_CA08 <- raw_new[raw_new$goat_id == "CA08" & raw_new$timestamp < "2023-10-05 06:30:38", ] #132
+dupe_CA09 <- raw_new[raw_new$goat_id == "CA09" & raw_new$timestamp < "2023-10-04 05:30:38", ] #122
+dupe_CA11 <- raw_new[raw_new$goat_id == "CA11" & raw_new$timestamp < "2023-10-05 06:30:38", ] #126
+dupe_CA12 <- raw_new[raw_new$goat_id == "CA12" & raw_new$timestamp < "2023-10-03 10:45:37", ] #121
+# 129 + 119 + 132 + 122 + 126 + 121 #= 749
+
+# chop the data, subset the data not to include 
+raw_new <- raw_new[!(
+  (raw_new$goat_id == "CA01" & raw_new$timestamp < "2023-10-05 06:30:38") |
+    (raw_new$goat_id == "CA04" & raw_new$timestamp < "2023-10-03 04:30:38") |
+    (raw_new$goat_id == "CA08" & raw_new$timestamp < "2023-10-05 06:30:38") |
+    (raw_new$goat_id == "CA09" & raw_new$timestamp < "2023-10-04 05:30:38") |
+    (raw_new$goat_id == "CA11" & raw_new$timestamp < "2023-10-05 06:30:38") |
+    (raw_new$goat_id == "CA12" & raw_new$timestamp < "2023-10-03 10:45:37")
+),]
+# 11628-10879 #= 749
+# 10879 fixes
+
+
+# check for duplicates to see if they were dropped correctly or if other duplicated fixes exist (some dupes are the same but with a difference of ~38-40secs in timestamp between the fixes)
+dupes <- raw_new[paste(raw_new$collar_id, raw_new$timestamp_utc) %in% 
+                   paste(raw_original$collar_id, raw_original$timestamp_utc), ] #14 without chopping, 0 when chopped
+
+# # drop duplicated rows
+# raw_new <- raw_new[!(paste(raw_new$collar_id, raw_new$timestamp_utc) %in%
+#                        paste(raw_original$collar_id, raw_original$timestamp_utc)),]
+
+# clean up environment
+rm(dupe_CA01, dupe_CA04, dupe_CA08, dupe_CA09, dupe_CA11, dupe_CA12, dupes)
+
+#........................................................
+
+# check for same column names to avoid duplication
 intersect(colnames(raw_original), colnames(raw_new))
 
 # combine datasets
 combined_data <- merge(raw_original, raw_new, 
                        by = c("goat_id", "goat_name", "collar_id", 
                               "timestamp_utc", "timestamp_local", "longitude", "latitude", 
-                              "fix_type", "data_source", "date_local", "outlier"),
+                              "fix_type", "data_source", "date_local", "outlier",
+                              "individual.local.identifier", "location.lat", "location.long", "timestamp"),
                        all = TRUE)
 
 #reset fix_id to account for new data
 rownames(combined_data) <- NULL
 combined_data$fix_id <- as.numeric(rownames(combined_data))
+combined_data <- relocate(combined_data, fix_id, .before = goat_id)
 
-str(combined_data)
-
-# create new columns to format names to match required for ctmm based on Movebank critera:
-combined_data$individual.local.identifier <- combined_data$goat_id
-combined_data$location.lat <- combined_data$latitude
-combined_data$location.long <- combined_data$longitude
-combined_data$timestamp <- combined_data$timestamp_utc
-
-
-#................................................
-
-# Convert collar data to ctmm telemetry object, and dont include screened outliers
-# must separate the datasets or will have convert issues with hdop and vdop values because one dataset has pdop and the other dop
-tel_data <- as.telemetry(combined_data[combined_data$data_source == 1,], mark.rm = FALSE,
-                         keep = c("fix_id", "goat_id", "goat_name", "collar_id", "data_source", "outlier"))
-
-
-# convert ctmm telemetry output list into a df
-tel_df <- do.call(rbind, tel_data)
-
-
-
-# Convert collar data to ctmm telemetry object, and dont include screened outliers
-tel_data2 <- as.telemetry(combined_data[combined_data$data_source == 2,], mark.rm = FALSE,
-                          keep = c("fix_id", "goat_id", "goat_name", "collar_id", "data_source", "outlier"))
-
-# convert ctmm telemetry output list into a df
-tel_df2 <- do.call(rbind, tel_data2)
-
-
-
-#............................................................
-# combine telemetry data together and sort by fix_id
-
-telemetry_df <- bind_rows(tel_df, tel_df2)
-telemetry_df <- telemetry_df[order(telemetry_df$fix_id),]
-
-
-#............................................................
-# merge main df with telemetry data
-
-# check for same column names to avoid duplication,
-intersect(colnames(combined_data), colnames(telemetry_df))
-
-# add telemetry data to main df
-combined_data <- merge(combined_data, telemetry_df, by = c("fix_id", "goat_id", "goat_name", "collar_id", "longitude",  "latitude", "timestamp", "data_source", "outlier"), all = TRUE)
-str(combined_data)
-
-#data carpentry
-combined_data <- combined_data[order(combined_data$fix_id),]
+# create a column to indicate what kind of dop type the fix had
+combined_data$dop_type <- as.factor(ifelse(!is.na(combined_data$dop), "dop",
+                                           ifelse(!is.na(combined_data$hdop), "hdop",
+                                                  ifelse(!is.na(combined_data$vdop), "vdop",
+                                                         ifelse(!is.na(combined_data$pdop), "pdop",
+                                                                NA)))))
+# organized the dops together
+combined_data <- relocate(combined_data, c(dop, dop_type), .after = pdop)
+combined_data <- relocate(combined_data, c(hdop, vdop, pdop, dop, dop_type), .after = fix_type)
 combined_data$fix_type <- as.factor(combined_data$fix_type)
-
 str(combined_data)
+
+# total raw dataset (10 animals) = 76978
+# indicate if they are part of this study or not
+combined_data$study_goat <- ifelse(combined_data$goat_id %in% c("CA01", "CA04", "CA08", "CA09", "CA11", "CA12"), 1, 0)
+sum(combined_data$study_goat) # 55194 fixes for the 6 goats
 
 save(combined_data, file = "./data/goat/prep/combined_data.rda")
+# load("./data/goat/prep/combined_data_w_dupes.rda") # previous version before chopping ~38-40sec timestamp difference but same everything else, refer to the screenshot of the df
 load("./data/goat/prep/combined_data.rda")
-
-
-
-
-
-#............................................................
-# merge tel_data together for a single list to be used in outlie filtering
-
-# check which goats are the same in both lists
-intersect(names(tel_data), names(tel_data2))
-
-# i.e., extract the df from list2, merge it to the df in list1
-tel_data[["CA01"]] <- merge(tel_data[["CA01"]], tel_data2[["CA01"]], all = TRUE)
-tel_data[["CA04"]] <- merge(tel_data[["CA04"]], tel_data2[["CA04"]], all = TRUE)
-tel_data[["CA08"]] <- merge(tel_data[["CA08"]], tel_data2[["CA08"]], all = TRUE)
-tel_data[["CA09"]] <- merge(tel_data[["CA09"]], tel_data2[["CA09"]], all = TRUE)
-tel_data[["CA11"]] <- merge(tel_data[["CA11"]], tel_data2[["CA11"]], all = TRUE)
-tel_data[["CA12"]] <- merge(tel_data[["CA12"]], tel_data2[["CA12"]], all = TRUE)
-
-# then order the df by fix_id
-tel_data[["CA01"]] <- tel_data[["CA01"]] [order(tel_data[["CA01"]][["fix_id"]]),]
-tel_data[["CA04"]] <- tel_data[["CA04"]] [order(tel_data[["CA04"]][["fix_id"]]),]
-tel_data[["CA08"]] <- tel_data[["CA08"]] [order(tel_data[["CA08"]][["fix_id"]]),]
-tel_data[["CA09"]] <- tel_data[["CA09"]] [order(tel_data[["CA09"]][["fix_id"]]),]
-tel_data[["CA11"]] <- tel_data[["CA11"]] [order(tel_data[["CA11"]][["fix_id"]]),]
-tel_data[["CA12"]] <- tel_data[["CA12"]] [order(tel_data[["CA12"]][["fix_id"]]),]
-
-str(tel_data[["CA08"]])
-
-
-save(tel_data, file = "./data/goat/prep/tel_data_merged.rda")
-load("./data/goat/prep/tel_data_merged.rda")
 
 
 
 # clean environment
 rm(list = ls())
 gc()
-
 
 
 
