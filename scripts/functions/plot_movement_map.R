17d6dc3b64fa3e2efa60a780a901c9454d166a3c
Chhen <aimee.chhen@ubc.ca>
2026-02-17 02:38:07 -0800

    added labels and popup to function, started adding the ability to click through different type of map tiles but having issues with it, need to troubleshoot that at some point maybe

--- scripts/functions/plot_movement_map.R
+++ scripts/functions/plot_movement_map.R
@@ -1,7 +1,14 @@
 
 
 
-# Esri.WorldTopoMap for elevation, Esri.WorldImagery for regular map image,
+
+
+
+# custom function to plot gps points over terrain (or some whatever leaflet has available) map
+# additionally using addCircleMarker() allows you to hover over the points on the map to get details of that point
+# to add a raster layer to the map, refer to https://rstudio.github.io/leaflet/articles/raster.html
+# Other maps to consider using: 'USGS.USTopo'
+# Esri.WorldTopoMap for elevation, Esri.WorldImagery for regular map image
 # any other leaflet tiles can be used here, refer to leaflet package for more information
 
 movement_map <- function(data, map_type = 'Esri.WorldImagery') {
@@ -13,7 +20,7 @@ movement_map <- function(data, map_type = 'Esri.WorldImagery') {
   if (is.null(map_source)) {
     stop(paste("Map provider", map_type, "not found."))
   }
-  
+
   # plot
   leaflet(data = data) %>%
     addProviderTiles(provider = map_type) %>%
@@ -30,9 +37,23 @@ movement_map <- function(data, map_type = 'Esri.WorldImagery') {
                      radius = 3,
                      fillOpacity = 0.8,
                      stroke = FALSE,
-                     label = ~as.character(data$timestamp)) %>% 
+                     # label and popup can modified to include whatever column info you want in to show)
+                     label = ~paste0("fix_id: ", fix_id, "  |  ", as.character(data$timestamp)),
+                     popup = ~paste0("fix_id: ", fix_id,
+                                     "  |  timestamp: ", as.character(data$timestamp),
+                                     "  |  lon: ", data$longitude,
+                                     "  |  lat: ", data$latitude,
+                                     "  |  x: ", round(x, 2),
+                                     "  |  y: ", round(y, 2))) %>%
+                       
+                     
     # add title (Note: when saving the plot, this title does not transfer over when using mapview::mapshot2)
     addControl(data$individual.local.identifier[1],
-               position = "topleft")
-  
+               position = "topleft") %>%
+    # add a layers control (ability to click through different map types) ***CURRENTLY DOESNT WORK NEED TO TROUBLESHOOT****, icon shows up but doesnt work
+    addLayersControl(baseGroups = c("OpenStreetMap",
+      "Esri.WorldStreetMap", "Esri.WorldTopoMap","Esri.WorldImagery",  "Esri.WorldTerrain", "Esri.OceanBasemap", "Esri.NatGeoWorldMap",
+      "CartoDB.Positron", "CartoDB.DarkMatter",
+      "USGS.USTopo", "USGS.USImagery", "USGS.USImageryTopo"))
+
 }
